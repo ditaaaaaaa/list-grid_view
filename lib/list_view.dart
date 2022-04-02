ca62840502fca253d98ffe2350d567052944e5ef
ditaaaaaaa <101062549+ditaaaaaaa@users.noreply.github.com>
2022-04-03 00:09:59 +0700

    Add files via upload

---
+++ lib/list_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:list_grid_view/circle.dart';
+import 'package:list_grid_view/square.dart';
+
+class mylistview extends StatelessWidget {
+  mylistview({Key? key}) : super(key: key);
+
+  final List _posts = ['post 1', 'post 2', 'post 3', 'post 4', 'post 5'];
+
+  final List _stories = [
+    'story 1',
+    'story 2',
+    'story 3',
+    'story 4',
+    'story 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('list view'),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 150,
+            child: Expanded(
+              child: ListView.builder(
+                itemCount: _stories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, indeex) {
+                  return mycircle(
+                    child: _stories[indeex],
+                  );
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _posts.length,
+              itemBuilder: (context, index) {
+                return mysquare(
+                  child: _posts[index],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
